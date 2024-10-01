d8cd61f99515fc80c9f9bd44b3ef0cd58afc4b35
jenraii <131862365+jenraii@users.noreply.github.com>
2024-10-01 18:21:37 +0300

    ImageService lisäys
    Tämä palveluluokka hakee kuvia Pixabay API:sta annetun hakusanan perusteella, tallentaa löydetyn kuvan URL:in ja hakusanan tietokantaan ja palauttaa tallennetun kuvan Image-objektina.

---
+++ src/main/java/op2/ryhmatyo/pelisivusto/service/ImageService.java
@@ -0,0 +1,43 @@
+package op2.ryhmatyo.pelisivusto.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.json.JSONObject;
+
+import java.util.Date;
+import java.util.Optional;
+import op2.ryhmatyo.pelisivusto.domain.Image;
+import op2.ryhmatyo.pelisivusto.domain.ImageRepository;
+
+@Service
+public class ImageService {
+
+    @Autowired
+    private ImageRepository imageRepository;
+
+    private final String PIXABAY_API_KEY = "46156034-444f3c2f3ed35bb2b993398bf";
+    private final String PIXABAY_URL = "https://pixabay.com/api/";
+
+    public Image searchAndSaveImage(String searchQuery) throws Exception {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = PIXABAY_URL + "?key=" + PIXABAY_API_KEY + "&q=" + searchQuery + "&image_type=photo";
+    
+        String response = restTemplate.getForObject(url, String.class);
+        JSONObject jsonResponse = new JSONObject(response);
+        
+        if (jsonResponse.getJSONArray("hits").length() > 0) {
+            String imageUrl = jsonResponse.getJSONArray("hits").getJSONObject(0).getString("webformatURL");
+    
+            // Tallenna kuva tietokantaan
+            Image image = new Image();
+            image.setImageUrl(imageUrl);
+            image.setSearchQuery(searchQuery);
+            
+            return imageRepository.save(image);
+        } else {
+            throw new Exception("Kuvaa ei löytynyt");
+        }
+    }
+    
+}
