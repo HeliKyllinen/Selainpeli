739b2e9bd86a3a9c4796130b00738f1703035245
sakariarasola <sagary_97@hotmail.com>
2024-10-31 14:43:30 +0200

    Palapelin muotoilu testi siirretty uuteen branchiin

--- src/main/java/op2/ryhmatyo/pelisivusto/WebSecurityConfig.java
+++ src/main/java/op2/ryhmatyo/pelisivusto/WebSecurityConfig.java
@@ -1,27 +1,51 @@
-/*package op2.ryhmatyo.pelisivusto;
+package op2.ryhmatyo.pelisivusto;
 
 import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+//import org.springframework.boot.web.servlet.dispatcherType;
 
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig {
 
     @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .cors().and()
+            .csrf().disable()
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(antMatcher("/css/**")).permitAll()
+                .anyRequest().authenticated())
+                //.dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+            .headers(headers -> headers.frameOptions(frameoptions -> frameoptions.disable()))
+            .formLogin() // Käytetään automaattista kirjautumissivua
+                .defaultSuccessUrl("/", true) // Kirjautumisen jälkeen ohjataan etusivulle
+                .permitAll()
+            .and()
+            .logout()
+                .permitAll();
+        return http.build();
+    }
+
+
+    @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
-
-}*/
+}
